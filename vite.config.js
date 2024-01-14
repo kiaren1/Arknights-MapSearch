156abdb822d0dad6a469528becee2c8e9fdfe67f
kiaren1 <1634602939@qq.com>
2024-01-14 18:41:00 +0800

    引入less，更改login中提交按钮

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { VantResolver } from '@vant/auto-import-resolver';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    Components({
+      resolvers: [VantResolver()],
+    }),],
 })
